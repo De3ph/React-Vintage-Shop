f3678bc40a22b4fcf93fee6b8d9541d55ce77be6
Ahmet Hakan Demirel <05190000043@ogrenci.ege.edu.tr>
2022-01-10 13:51:06 +0300

    Footerdaki sayfa düzenlemeleri

---
+++ src/pages/About.js
@@ -0,0 +1,9 @@
+function About({}){
+    return(
+        <main>
+            <h2 className="text-center animate__animated animate__rotateInDownRight">About Us</h2>
+            <div></div>
+        </main>
+    );
+}
+export default About;
