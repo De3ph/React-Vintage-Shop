867c7d1c8272568c0c248328a0b8636007ced93a
yekta.yuksel <yekta.yuksel@gmail.com>
2022-01-13 17:12:51 +0300

    buy form redesigned

--- src/pages/mainContents/BuyingPage.js
+++ src/pages/mainContents/BuyingPage.js
@@ -22,36 +22,36 @@ const BuyinPage = ({cart , setCart}) => {
 
        <form className="row g-3 user-info">
 
-           <div className="col-md-6">
-               <label for="name" className="form-label">Name</label>
+           <div className="col-lg-6 col-md-6 ">
+               <label for="name" className="form-label fs-5">Name</label>
                <input className="form-control" id = "name" />
            </div>
-           <div className="col-md-6">
-               <label for="surname" className="form-label">Surname</label>
+           <div className="col-lg-6 col-md-6">
+               <label for="surname" className="form-label fs-5">Surname</label>
                <input className="form-control" id = "surname" />
            </div>
            <div className="col-md-12">
-               <label for="inputEmail4" className="form-label">Email</label>
+               <label for="inputEmail4" className="form-label fs-5">Email</label>
                <input  className="form-control" id = "inputEmail4" />
            </div>
            <div className="col-md-12">
-               <label for="address" className="form-label">Address</label>
+               <label for="address" className="form-label fs-5">Address</label>
                <input className="form-control" id="address" />
            </div>
            <div className="col-md-6">
-               <label for="card" className="form-label">Card Number</label>
+               <label for="card" className="form-label fs-5">Card Number</label>
                <input className="form-control" id="card" placeholder="0000-0000-0000-0000"/>
            </div>
            <div className="col-md-3">
-               <label for="cvc" className="form-label">CVC</label>
-               <input className="form-control" id="cvc"/>
+               <label for="cvc" className="form-label fs-5">CVC</label>
+               <input className="form-control" id="cvc" placeholder="000"/>
            </div>
            <div className="col-md-3">
-               <label for="date" className="form-label">Expiration Date</label>
-               <input className="form-control" id="date"/>
+               <label for="date" className="form-label fs-5">Expiration Date</label>
+               <input className="form-control" id="date" placeholder="00/00"/>
            </div>
            <p>Total Price: {total}$</p>
-           <button onClick={handleSubmit} type="submit" className="btn btn-lg btn-success fs-2 submitButton">Buy</button>
+           <button onClick={handleSubmit} type="submit" className="btn btn-lg btn-warning fs-2">Buy</button>
 
             {true && isRedirect ? <Redirect to="/cart" /> : null}
        </form>
