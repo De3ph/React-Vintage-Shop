8b2418ddfb5f4496ca1d97424648dfdb7193258b
kmlisler <82888052+kmlisler@users.noreply.github.com>
2022-01-13 16:06:47 +0300

    About sayfası düzenlendi

--- src/pages/footerPages/About.js
+++ src/pages/footerPages/About.js
@@ -1,8 +1,18 @@
 function About() {
     return (
         <main>
-            <h2 className="text-center animate__animated animate__rotateInDownRight">About Us</h2>
-            <div style={{textAlign : "center"}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad esse, molestiae sunt incidunt iusto, voluptate, illo sapiente placeat illum vero veniam voluptatem ipsum.</div>
+            <h2 className="text-center animate__animated animate__rotateInDownRight locationHeader">About Us</h2>
+            <br/>
+            <div style={{textAlign : "center"}}>
+                <p style = {{fontSize :"150%"}}>Although not as old as antiques, vintage is an adjective used for old products that have had their share of fashion, such as clothing, clothing, and cars for a few decades.
+
+                    Vintage is old, but since it is not more than 100 years old, it cannot be considered an antique. If the age of an item is not more than 100 years, it is considered vintage.
+                </p>
+                <div style ={{fontSize :"150%"}}>
+                <p style = {{color : "#FFC107"}}>We sell you this web site vintage products.</p>
+                </div>
+                
+            </div>
         </main>
     );
 }
